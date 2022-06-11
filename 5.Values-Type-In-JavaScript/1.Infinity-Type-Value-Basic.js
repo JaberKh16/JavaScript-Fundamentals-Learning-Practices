8af8e3d76ffcd3053438f0fac081213cdd6fed50
Jaber Khan <md.jaberkhan66@gmail.com>
2022-06-12 04:34:40 +0600

    Types-Of-Available-Values-Concept-In-JS

---
+++ 5.Values-Type-In-JavaScript/1.Infinity-Type-Value-Basic.js
@@ -0,0 +1,6 @@
+// Declaring 'use strict' statement
+'use strict';
+
+// Declaring A Variable
+let number = 1/0;  // this mathematical expression will results 'Infinity' in JS
+console.log(number);
---
+++ 5.Values-Type-In-JavaScript/2.NaN-Type-Value-Basic.js
@@ -0,0 +1,6 @@
+// Declaring 'use strict' statement
+'use strict';
+
+// Declaring A Variable
+let invalid_expression = "adhar"/3;  // this invalid expression will results 'NaN' in JS
+console.log(invalid_expression);
