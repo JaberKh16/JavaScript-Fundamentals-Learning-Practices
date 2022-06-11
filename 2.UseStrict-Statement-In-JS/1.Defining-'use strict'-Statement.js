211994b78f6eac5921547c48e24f3a56c312f21d
Jaber Khan <md.jaberkhan66@gmail.com>
2022-06-12 04:29:17 +0600

    'use strict'-Statement-Concept-In-JS

---
+++ 2.UseStrict-Statement-In-JS/1.Defining-'use strict'-Statement.js
@@ -0,0 +1,18 @@
+/*
+    'use strict' in Javascript is basically the statement which is used
+    to define or mentioning that this script file is following Ecmascript(ES5)
+    property/specification provided by the modern browser. It ensures the use 
+    of JavaScript file being used all modern basde properites and specification.
+    To create the statement use the following:
+        'use strict'   or "use strict"
+    which means that single quotes or double quotes can be used to declare that.
+    
+    And this statement can be define anywhere inside the script file, but best 
+    practice is to use it on top of any JS Code because whatever JS Code after can
+    only consider the ES5 specification.
+
+*/
+
+'use strict'
+
+alert("something is fissy..");
