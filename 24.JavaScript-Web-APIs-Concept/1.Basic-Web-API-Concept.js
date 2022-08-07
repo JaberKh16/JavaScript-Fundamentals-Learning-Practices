3650b94ab54ad0246c948b2fb6ab291d62e64e50
Jaber Khan <md.jaberkhan66@gmail.com>
2022-08-07 09:13:09 +0600

    JavaScript-Web-APIs

---
+++ 24.JavaScript-Web-APIs-Concept/1.Basic-Web-API-Concept.js
@@ -0,0 +1,50 @@
+/*
+    JavaScript Web APIs Concept
+    ===========================
+    Before understanding Web APIs, first discuss what API is?- API
+    stands for Application Programming Interface which holds some
+    functionality to do some specific sort of works while being
+    request.
+
+    API is basically a software intermediary that allows two applications 
+    to talk to each other. 
+    For Example- Each time you use an app like Facebook, send an instant 
+    message, or check the weather on your phone. 
+    How? - Through using an API.
+
+    Now, An Web API is an API for the Web Only.
+
+    Advantage of Web API
+    --------------------
+    1) can extends the functionality of a Browser.
+    2) can greatly simplify the complex functions.
+    3) can provide easy syntax to complex code.
+    
+    There are two types of Web API for Browser-
+        1) Browser APIs
+        2) Third Party APIs
+
+    Browser APIs
+    ------------
+    All browsers have a set of built-in Web APIs to support complex operations, 
+    and to help accessing data.
+    
+    For Example- Geolocation API can return the coordinates of where the 
+    browser is located.
+
+
+    Third Party APIs
+    ----------------
+    Third party APIs are not built into your browser. To use these APIs, 
+    will have to download the code from the Web.
+
+    For Examples:
+
+    a) YouTube API - Allows you to display videos on a web site.
+    b) Twitter API - Allows you to display Tweets on a web site.
+    c) Facebook API - Allows you to display Facebook info on a web site.
+
+*/
+
+// defining the 'use strict' statement
+'use strict';
