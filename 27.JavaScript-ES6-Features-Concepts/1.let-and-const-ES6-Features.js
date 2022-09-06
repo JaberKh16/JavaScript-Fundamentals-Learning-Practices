638ba037672ee55a0a1948cf448621baa938e5a7
Jaber Khan <md.jaberkhan66@gmail.com>
2022-09-06 20:26:09 +0600

    ES6-Features-In-JS

---
+++ 27.JavaScript-ES6-Features-Concepts/1.let-and-const-ES6-Features.js
@@ -0,0 +1,10 @@
+/*
+    JavaScript ES6 Features - let/const
+    ===================================
+    JavaScript ES6 comes up with a new way of declaring variables
+    through "let" and "const". Both of them have Block Scope in
+    terms of Scoping in JS.
+
+*/
+
+// ES5 way of declaring variable
