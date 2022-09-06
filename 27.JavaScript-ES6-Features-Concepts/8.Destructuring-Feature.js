3c9d4590e3321319c6e720512d91f5be971ad842
Jaber Khan <md.jaberkhan66@gmail.com>
2022-09-07 02:22:51 +0600

    ES6-Features-In-JS

---
+++ 27.JavaScript-ES6-Features-Concepts/8.Destructuring-Feature.js
@@ -0,0 +1,39 @@
+/*
+    JavaScript ES6 Features- Destructuring 
+    ======================================
+    In ES6 feature of Destructuring any objects or array can element
+    can be destructured.
+    For Example-
+        // destructuring array element
+        const arr = [10, 20, 40, 50];
+        const [v1, v2] = arr; // stores v1=10, v2=20
+
+        // destructuring objects properties
+        const obj = {
+            property1: value1,
+            property2: value2,
+            property3: function (){
+
+            },
+        }
+        const {v1, v2} = obj; // stores v1 = value1, v2 = value3 
+
+
+*/
+
+// destructuring array elements
+const fruits = ['banana', 'apple', 'guava', 'lichie'];
+const [banana, apple, ...otherFruits] = fruits;
+console.log(banana, apple, otherFruits);
+
+// destructuring objects properties
+const personInfo = {
+    name: "Jonathan Smith",
+    age: 34, 
+    gender: 'Male',
+    profession: "Programmer",
+    languageKnown: ['C++', 'Java', 'JavaScript', 'Python']
+}
+
+const {personName, personAge, ...personOtherInfo} = personInfo;
+console.log(personInfo.personName, personInfo.personAge, personOtherInfo);
