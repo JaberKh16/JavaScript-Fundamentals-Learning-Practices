b77b1543030182dab45e1f2503471ce48f752ff2
Jaber Khan <md.jaberkhan66@gmail.com>
2022-09-06 22:34:11 +0600

    ES6-Features-In-JS

--- 27.JavaScript-ES6-Features-Concepts/5.'spread'-Operator-Feature.js
+++ 27.JavaScript-ES6-Features-Concepts/5.'spread'-Operator-Feature.js
@@ -8,9 +8,9 @@
 
         const arr1 = [1, 2, 5, 6];
         const arr2 = [10, 20, 40, 32]
-        const arr3 = [];
-        arr3.concat(arr1);
-        arr3.concat(arr2);
+        const arr3 = arr1.concate(arr2)
+        const arr4 = arr3.concate([100, 200, 300])
+
 
     ES6 'spread' Operator can be defined through the following-
         const arr1 = [1, 2, 5, 6];
@@ -24,11 +24,15 @@
 // Example-1 ES5 way of working
 var arr1 = [1, 4, 6, 7, 8];
 var arr2 = [10, 25, 79, 91];
-var arr3 = [];
+var arr3 = arr1.concat(arr2);
+var arr4 = arr3.concat([100, 200, 300]);
 
-arr1.concat(arr2);
-arr3 = arr1;
-console.log(arr3);
+console.log("Array3: ", arr3);
+console.log("Array4: ", arr4);
 
-arr3.concat(arr2)
-console.log(arr3);
+
+// Example-2 ES6 way of working
+const arr11 = [1, 4, 6, 7, 8];
+const arr22 = [10, 25, 79, 91]; 
+const arr33 = [...arr11, ...arr22];
+console.log("Array33: ", arr33);
