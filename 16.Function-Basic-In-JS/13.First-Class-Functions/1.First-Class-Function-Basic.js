2a083c1e3931030c8c031adab63ff83f79b6d1ea
Jaber Khan <md.jaberkhan66@gmail.com>
2022-09-06 17:00:19 +0600

    JavaScript-Functions-Concepts

---
+++ 16.Function-Basic-In-JS/13.First-Class-Functions/1.First-Class-Function-Basic.js
@@ -0,0 +1,39 @@
+/*
+    First Class Functions Basic Concept
+    ===================================
+    First Class Function are those function which are being passed
+    as an arguments to another function call(invokation) or called 
+    parameters when defining the function. So, if a function is
+    passed inside another function as arguments then those arguemnts
+    are nothing but the values(returned result) and when functions
+    are treated like values then those passed functions are 
+    considered as "First Class Functions". 
+
+    For Example-
+        let sum = function(para){
+            console.log(para);
+        }
+        sum(function(){
+
+        });
+
+*/
+
+// defining the 'use strict' statement
+'use strict';
+
+
+function gettingRadiusValue(){
+    const radius = 5;
+    return radius;
+}
+
+function findingAreaOfCircle(gettingRadiusValue){
+    return 2 * Math.PI * gettingRadiusValue();
+}
+
+// invoking another function to this function as an argument passing
+const areadOfCircle =  findingAreaOfCircle(gettingRadiusValue);
+console.log(areadOfCircle);
+
+
