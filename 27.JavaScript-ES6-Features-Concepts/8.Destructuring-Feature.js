a82356254971826890930018b14169b4714c4ede
Jaber Khan <md.jaberkhan66@gmail.com>
2022-09-07 02:43:35 +0600

    ES6-Features-In-JS

--- 27.JavaScript-ES6-Features-Concepts/8.Destructuring-Feature.js
+++ 27.JavaScript-ES6-Features-Concepts/8.Destructuring-Feature.js
@@ -16,45 +16,48 @@
 
             },
         }
-        const {v1, v2} = obj; // stores v1 = value1, v2 = value3 
+        const {property1, property2} = obj; // stores v1 = value1, v2 = value3 
 
-
+    When destructuirng objects it is necessary to have the same exact property
+    name while destructuring those property values, otherwise new name will
+    considered as new variable thus values of those properties will be 
+    'undefined'.
 */
 
 // destructuring array elements
 const fruits = ['banana', 'apple', 'guava', 'lichie'];
-const [banana, apple, ...otherFruits] = fruits;
-console.log(banana, apple, otherFruits);
+const [fruit1, fruit2, ...otherFruits] = fruits;
+console.log(fruit1, fruit2, otherFruits);
 
 // destructuring objects properties
-let personInfo = {
-    name: "Jonathan Smith",
-    age: 34, 
+const personInfo = {
+    personName: "Jonathan Smith",
+    personAge: 34, 
     gender: 'Male',
     profession: "Programmer",
     languageKnown: ['C++', 'Java', 'JavaScript', 'Python']
 }
 
 const {personName, personAge, ...personOtherInfo} = personInfo;
-console.log(personInfo.personName, personInfo.personAge, personOtherInfo);
+console.log(personName, personAge, personOtherInfo);
 
 // destructuring object & nested object & combine object into single object
 let userInfo = {
-    name: 'Mike',
-    friend: ["John", "Paul", "Jimmy"],
-    location: {
+    UserName: 'Mike',
+    UserFriend: ["John", "Paul", "Jimmy"],
+    UserLocation: {
         region:"England",
         country:"United Kingdom"
     },
-    aboutMe: {
+    UserAboutMe: {
         status: "Single",
         pet: "Dog",
     }
 }
 
-const { userName, userFriend, userLocation } = userInfo;
+const { userName, userFriend, userLocation, userAboutMe:{status, pet} } = userInfo;
 console.log(userName); // output: "Mike"
 console.log(userFriend);  // output: [ 'John', 'Paul', 'Jimmy' ]
 console.log(userLocation); // output: { region: 'England', country: 'United Kingdom' }
-// console.log(status); // output: "Single"
-// console.log(pet); // output: "Dog"
+console.log(UserAboutMe); // output: "Single"
+console.log(UserAboutMe.pet); // output: "Dog"
