c2ed67d146a42bacdcf5cad9874eb08eb0621459
Jaber Khan <md.jaberkhan66@gmail.com>
2022-09-06 23:34:51 +0600

    ES6-Features-In-JS

--- 27.JavaScript-ES6-Features-Concepts/7.For-of-Loop-Feature.js
+++ 27.JavaScript-ES6-Features-Concepts/7.For-of-Loop-Feature.js
@@ -11,13 +11,28 @@ buttonClicked1.addEventListener('click', function(){
     });
 })
 
-// Example-2 ES6 way of dealing with HTMLCollection
+// // Example-2 ES6 way of dealing with HTMLCollection
+// Solvance-1
+// const buttonClicked2 = document.getElementById('click-button2');
+// buttonClicked2.addEventListener('click', function(){
+//     const listItems = document.getElementsByClassName('list-item2');
+//     console.log(listItems);
+//     // converting HTMLCollection to an Array  
+//     Array.from(listItems).forEach(element => {
+//         element.classList.toggle('list-item_whenclick2');
+//     });
+// })
+
+
+// Example-2 ES6 way of dealing with HTMLCollection with For Of Loop
+// Solvance-2
 const buttonClicked2 = document.getElementById('click-button2');
 buttonClicked2.addEventListener('click', function(){
     const listItems = document.getElementsByClassName('list-item2');
     console.log(listItems);
     // converting HTMLCollection to an Array  
-    Array.from(listItems).forEach(element => {
+    const convertedArray = Array.from(listItems);
+    for(let element of convertedArray){
         element.classList.toggle('list-item_whenclick2');
-    });
+    };
 })
