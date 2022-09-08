3059993c8bf1c30c2f4a682915c554bd83d06d2e
Jaber Khan <md.jaberkhan66@gmail.com>
2022-09-08 16:55:33 +0600

    JavaScript-DOM-Based-Concepts

--- 18.DOM-Based-Concept-In-JS/9.DOM-HTMLElement-Objects-Properties-Concept/1.HTMLElement-Objects-Basic.js
+++ 18.DOM-Based-Concept-In-JS/9.DOM-HTMLElement-Objects-Properties-Concept/1.HTMLElement-Objects-Basic.js
@@ -17,16 +17,17 @@
     i)  document.documentURI            --> returns the URI of the document.
     j)  document.domain                 --> returns the domain name of the document server.
     k)  document.embeds                 --> returns the <embed> element.
-    l)  document.forms                  --> returns the <form> element.
+    l)  document.forms                  --> returns the <form> element as a HTMLCollection.
     m)  document.head                   --> returns the <head> element.
     n)  document.images                 --> returns the <img> element.
     o)  document.implementation         --> returns the DOM implementation.
     p)  document.inputEncoding          --> returns the document's encoding.
     q)  document.lastModified           --> returns the date and time the document was last updated.
-    r)  document.links                  --> returns the <area> and <a> elements that have a 'href' attribute.
+    r)  document.links                  --> returns the <area> and <a> elements that have 
+                                            a 'href' attribute as a HTMLCollection.
     s)  document.readyState             --> returns the loading state of the document.
     t)  document.referrer               --> returns the URI of the referrer(the linking document).
-    u)  document.scripts                --> returns the <script> elements.
+    u)  document.scripts                --> returns the <script> elements as a HTMLCollection.
     v)  document.title                  --> returns the <title> element.
     w)  document.strictErrorChecking    --> returns if error checking is enforced.
     x)  document.URL                    --> returns the complete URL of the document.
@@ -37,5 +38,13 @@
 // defining the 'use strict' statement
 'use strict';
 
-console.log(document.scripts); // to see the scripts 
-console.log(document.links);  // to see the links 
+console.log(document.scripts); // to see the scripts as a HTMLCollection
+console.log(document.links);   // to see the links as a HTMLCollection
+console.log(document.URL);    // to see the file URL
+console.log(document.title);  // to see the <title> tag
+console.log(document.head);  // to see the <title> tag
+console.log(document.forms);  // to see the <form> tag as a HTMLCollection
+console.log(document.body);  // to see the <body> tag
+console.log(document.documentURI);  // to see the document URI
+console.log(document.documentElement);  // to see the whole document
+console.log(document.domain);       // to see the domain 
