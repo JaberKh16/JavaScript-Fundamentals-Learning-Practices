f58d75b5b8bde699b727cf162c36f3af7cf0b25e
Jaber Khan <md.jaberkhan66@gmail.com>
2022-09-08 16:58:19 +0600

    JavaScript-DOM-Based-Concepts

---
+++ 18.DOM-Based-Concept-In-JS/7.HTMLElementObject-Based-Methods-Concept/4.document.replaceChild().js
@@ -0,0 +1,10 @@
+/*
+    HTML Element Object Based Method- document.replaceChild()
+    ==========================================================
+    document.replaceChild() is used to replace a HTML Element.
+    This method replace one HTML element with another HTML element.
+    
+    Syntax-
+        document.replaceChild(new, old);
+
+*/
