fdb3f66679e81803a77139355a4a73c97055b8e6
Jaber Khan <md.jaberkhan66@gmail.com>
2022-09-08 19:39:40 +0600

    JavaScript-Functions-Concepts

--- 16.Function-Basic-In-JS/7.Self-Invoking-Function(IIFE)-Basic/1.Self-Invoking-Function-Basic.js
+++ 16.Function-Basic-In-JS/7.Self-Invoking-Function(IIFE)-Basic/1.Self-Invoking-Function-Basic.js
@@ -1,9 +1,8 @@
 /*
     Self Invoking Function Concept
     ==============================
-    Self Invoking Function In JS are those function which 
-    will make them call themselves. Self Invoking Function
-    also known as IIFE(Immediately Invoking Function Expression).
+    Self Invoking Function In JS are those function which  will make them call themselves. 
+    Self Invoking Function also known as IIFE(Immediately Invoking Function Expression).
     
     Syntax:
         (funtion (){
@@ -11,23 +10,20 @@
         })();
 
 
-    To self invoke a function add a () after the function
-    definition is done, before the semicolon.
+    To self invoke a function add a () after the function definition is done, 
+    before the semicolon.
 
     Use Case Of IIFE
     -----------------
-    IIFE best use case is whenever there's having a similar named
-    variables and those variables are defined on the Global Scope
-    then there's possiblities of changing of values. Imagine a 
-    scenario-
+    IIFE best use case is whenever there's having a similar named variables and those 
+    variables are defined on the Global Scope then there's possiblities of changing of values. 
+    Imagine a scenario-
         
-    Though Vanila JS doesn't support modularity thus one developer
-    say write a file where he used some variable named as - 'a'
-    in the Glbal Scope. Now, another developer in his file he also
-    uses 'a' variable to do his logical work now the first developer
-    'a' variable gets changes with the value of second developer writtem
-    'a' variable, which cause problem. To avoid this type of issues
-    JS introduce IIFE.
+    Though Vanila JS doesn't support modularity thus one developer say write a file where 
+    he used some variable named as - 'a' in the Glbal Scope. Now, another developer 
+    in his file he also uses 'a' variable to do his logical work now the first developer
+    'a' variable gets changes with the value of second developer written 'a' variable, 
+    which cause problem. To avoid this type of issues JS introduce IIFE.
 
 
     Note: Can't make a normal function declaration self invoking.
@@ -40,8 +36,7 @@
 // Declaring a self invoking function
 let number = 1;
 let counter = (function(){
-    for(let number=1; number <=10; number++)
-    {
+    for(let number=1; number <=10; number++){
         console.log(`${number}`);
     }
 })();
--- 16.Function-Basic-In-JS/9.Arguments-Object-Basic/1.Arguments-Object-Basic.js
+++ 16.Function-Basic-In-JS/9.Arguments-Object-Basic/1.Arguments-Object-Basic.js
@@ -1,21 +1,37 @@
 /*
     Arguments Objects Concept
     =========================
-    Arguments Objects generally used to hold multiple values as
-    a object while passing multiple parameters through a function
-    as single a parameter which is basically a object.
+    Arguments Objects generally used to hold multiple values as an object while passing 
+    multiple parameters to a function as single a parameter which is basically a object.
+
+    So, basically when need to pass multipl no. of arguments as parameters to the function
+    where the issue is that- don't know the exact no. of arguments need to be passed that
+    when 'arguments' object comes to play.
+
+    'arguments' objects is an array like having two values which are the following-
+        1) Callee Function --> holds the reference of the function
+        2) Symbol Function 
+
 
     Syntax:
         function functionName(){
-            for(let i =0; i<arguments.length; i++)
-            {
+            for(let i =0; i<arguments.length; i++){
                 arguments[i];
             }
         };
+    
+
+    'arguments' is seems like array, but it is not an actual array which can be checked 
+    through the following code-
+        
+            console.log(arguments instanceof Array); // retunrs 'false'
+
 
     To access the arguments objects use the following:
                 
             arguments.length  ; to get the all values though take multiple values as object
+
+    
 */
 
 // Declaring 'use strict' statement
@@ -29,6 +45,7 @@ function valuesList(){
         summation += arguments[i];
     }
     console.log(`Sum Value is: ${summation}`);
+    console.log(arguments); // to see the arguments array-like object
 };
 
 // Create a variable with multiple values
