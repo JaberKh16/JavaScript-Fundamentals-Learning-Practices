71d62ada59dd147dc2dc803f4f7e9f17ead6aa3a
Jaber Khan <md.jaberkhan66@gmail.com>
2022-09-10 02:04:14 +0600

    JavaScript-BOM-Based-Concepts

--- 19.BOM-Based-Concept-In-JS/7.JavaScript-TimingEvents-In-BOM.js
+++ 19.BOM-Based-Concept-In-JS/7.JavaScript-TimingEvents-In-BOM.js
@@ -1,3 +1,13 @@
 /*
-    Ja
+    JavaScript BOM Based Object - TimingEvents Object
+    =================================================
+    TimingEvents is one of the BOM tools to work with the timing related events.
+    BOM has 4 types of timing events which are the following-
+        
+        1) setTimeout(callback, timer)      ---> to set a timer for a function to call for once.
+        2) setInterval(callback, timer)     ---> to set a timer for a function to call repeatedly.
+        3) clearTimeout(timervariable)      ---> to clear the setTimout().
+        4) clearInterval(timervariable)     ---> to clear the setInterval().
+
 */
+
