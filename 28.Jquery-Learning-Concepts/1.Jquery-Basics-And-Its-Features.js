0c04018d69d6770db18f9eb6ccf001658217367d
Jaber Khan <md.jaberkhan66@gmail.com>
2022-09-11 19:02:41 +0600

    Jquery-Based-Learning-Materials

---
+++ 28.Jquery-Learning-Concepts/1.Jquery-Basics-And-Its-Features.js
@@ -0,0 +1,45 @@
+/*
+    JavaScript Jquery Based Concepts
+    ================================
+    Jquery is a JS library that simplifies different VanilaJS(raw JS) features such as
+    DOM Manipulation, Ajax Request Handling.
+
+    Jqury Syntax-
+        $('selector').someMethod('parameter');
+    
+    In the above syntax, 
+        1) $ --> access modifier of Jquery
+        2) selector     ---> to select HTML Element
+        3) someMethod   ---> some methods on selected elements provided by Jquery
+        4) parameter    ---> some optional paramter
+
+    Advantages Of Jquery
+    --------------------
+    1) Simple, easy and light weight.
+    2) Provides HTML & CSS Manipulation.
+    3) Provides DOM Manipulation.
+    4) Has Built-in Animation support.
+    5) Has cross-browser support.
+    6) Has AJAX support.
+    7) Provides Event Handling support.
+
+    Concepts In Jquery
+    ------------------
+    1)  Jquery Selector
+    2)  Jquery Events
+    3)  Jquery Callbacks
+    4)  Jquery Animation
+    5)  Jquery Chaining
+    6)  Jquery CSS Methods
+    7)  Jquery Traversing
+    8)  Jquery Ancestor
+    9)  Jquery Siblings
+    10) Jquery Filtering
+    11) Jquery AJAX
+
+    How To Work With Jquery
+    -----------------------
+    There are 2 ways to have Jquery on your syetem and emebed with HTML which are the following-
+        1) Downloading the compressed or uncompressed file from Jquery sites(best for production) 
+        2) Using Jquery CDN(get from 'Google CDN', 'Microsoft CDN', 'CDNJS', 'jsDelivr CDN')
+*/
