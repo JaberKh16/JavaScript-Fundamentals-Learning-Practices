4f199c6d997c90f957b7d6351bc4377168ab05c3
Jaber Khan <md.jaberkhan66@gmail.com>
2022-09-11 21:15:26 +0600

    Jquery-Based-Learning-Materials

---
+++ 28.Jquery-Learning-Concepts/2.Jquery-Selectors-Basic.js
@@ -0,0 +1,54 @@
+/*
+    Jquery Selectors Concept
+    ========================
+    Jquery Selector can use to select and manipulate HTML Elements.
+    
+    Syntax Of Jquery Selectors
+    --------------------------
+        $('selector').method();
+
+    Selecting Ways Of Jquery Selectors
+    ----------------------------------
+    1) $('h2')              --> selected through a HTML tag(element) name.
+    2) $('.className')      --> selected through a class name.
+    3) $('#idName')         --> selected through a id name.
+    4) $('[href]')          --> selected through a attribute name.
+    5) $('a[href='..']')    --> selected through a attribute value.
+    6) $('p .classTitle')   --> selected through psuedo selection.
+    7) $('p #idTitle')      --> selected through psuedo selection.
+    
+    Attributes Based Selectors
+    --------------------------
+    1) [Attribute]         ==> $('[href]')  --> selected all elements with 'href' attribute.
+    2) [Attribute=value]   ==> $(':empty)   --> selected all elements no child(element) nodes.
+    3) [Attribute!=value]  ==> $('[href!='default.html']) --> selected all elements with 'href' attribute
+                                value not equal to 'default.html'.
+    4) [Attribute$=value]  ==> $('[href$='.png']') --> selected all element with 'href' attribute
+                                value ends with '.png'.
+    5) [Attribute^=value]  ==> $('[href^='somefile_']') --> selected all element with 'href' attribute value 
+                                starts with 'somefile_'.
+
+    Form Based Selectors
+    ---------------------
+    1) $(':input')         --> selected all input elements.
+    2) $(':text')         --> selected all input elements with type='text'
+    3) $(':password')         --> selected all input elements with type='password'
+    4) $(':radio')         --> selected all input elements with type='radio'
+    4) $(':checkbox')         --> selected all input elements with type='checkbox'
+    4) $(':submit')         --> selected all input elements with type='submit'
+    4) $(':reset')         --> selected all input elements with type='reset'
+
+    Others Different Selectors
+    --------------------------
+    1)  :first              ==> $('p:first')        --> select first 'p' element. 
+    2)  :last               ==> $('p:last')         --> select last 'p' element.
+    3)  :even               ==> $('tr:even')        --> select all even 'tr' element.
+    4)  :odd                ==> $('li:odd')         --> select all odd 'li' element.
+    5)  :gt(num)            ==> $('ui li:gt(2)')    --> select elements with an index greater than 2.
+    6)  :lt(num)            ==> $('ui li:lt(3)')    --> select elements with an index less than 3.
+    7)  :eq(index_val)      ==> $('ui li:eq(2)')    --> select 3rd element in the list(index value starts from 0).
+    8)  :header             ==> $(':header')        --> select all headers h1, h2, ..
+    9)  :animated           ==> $(':animated')      --> select all animated elements.
+    10) S1, S2, S3          ==> $('th td p')        --> select all elements with matching selectors.
+    
+*/
