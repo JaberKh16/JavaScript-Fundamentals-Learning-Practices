8d2fbeea282ec070b5ff7f8c95810d57d09c359a
Jaber Khan <md.jaberkhan66@gmail.com>
2022-09-13 03:53:57 +0600

    Jquery-Based-Learning-Materials

---
+++ 28.Jquery-Learning-Concepts/4.Jquery-Events-Based-Concepts.js
@@ -0,0 +1,26 @@
+/*
+    Jquery Events Based Concepts
+    ============================
+    Jquery provides some simple methods to work with events basically those methods attaches
+    the event handlers(nothing but the function) to do particular task. Those handlers are 
+    being called upon when the selector for that handler being triggered.
+
+    Jquery Events Based Method Types
+    --------------------------------
+    a) Mouse Events Methods
+    b)
+
+    Mouse Events Methods
+    --------------------
+    1)  click()         --> triggered by mouse click.
+    2)  dbclick()       --> triggered by mouse double click.
+    3)  mousedown()     --> fires when mouse button is pressed.
+    4)  mouseup()       --> fires when mouse button is released.
+    5)  mouseenter()    --> called when the mouse enters the html area.
+    6)  mouseleave()    --> called when the mouse leaves     the html area.
+    7)  hover()         --> fires when the mouse enters and leave the the html area.
+    8)  mousemove()     --> fires when the mouse is moved.
+    9)  mouseout()      --> fires when the mouse moves out of the html area.
+    10) mouseover()     --> fires when the mouse moves over the html area.
+
+*/
