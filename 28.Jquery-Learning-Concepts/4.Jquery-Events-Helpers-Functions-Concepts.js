361b5da7ee64f2d8912ee579448728e708bd039e
Jaber Khan <md.jaberkhan66@gmail.com>
2022-09-13 07:02:12 +0600

    Jquery-Based-Learning-Materials

--- 28.Jquery-Learning-Concepts/4.Jquery-Events-Helpers-Functions-Concepts.js
+++ 28.Jquery-Learning-Concepts/4.Jquery-Events-Helpers-Functions-Concepts.js
@@ -1,6 +1,6 @@
 /*
-    Jquery Events Based Concepts
-    ============================
+    Jquery Events Helper Functions Concepts
+    =======================================
     Jquery provides some simple methods to work with events basically those methods attaches
     the event handlers(nothing but the function) to do particular task. Those handlers are 
     being called upon when the selector for that handler being triggered. Insied this
@@ -16,15 +16,16 @@
     a) Mouse Events Methods
     b) Form Events Methods
     c) Documents Events Methods
+    d) Keyboard Events Methods
 
     Mouse Events Methods
     --------------------
     1)  click()         --> triggered by mouse click.
-    2)  dbclick()       --> triggered by mouse double click.
+    2)  dblclick()      --> triggered by mouse double click.
     3)  mousedown()     --> fires when mouse button is pressed.
     4)  mouseup()       --> fires when mouse button is released.
     5)  mouseenter()    --> called when the mouse enters the html area.
-    6)  mouseleave()    --> called when the mouse leaves     the html area.
+    6)  mouseleave()    --> called when the mouse leaves the html area.
     7)  hover()         --> fires when the mouse enters and leave the the html area.
     8)  mousemove()     --> fires when the mouse is moved.
     9)  mouseout()      --> fires when the mouse moves out of the html area.
@@ -56,6 +57,29 @@
     3) keydown()    --> triggers when any pressed is being pressed down.
 
 */
+
+$('document').ready(function (){
+    $('.mouse-events__methods').click(function(){
+        $('.list-items  li:first-child').css('color', 'green');
+    });
+    $('.mouse-events__methods').dblclick(function(){
+        $('.list-items > li').filter(':gt(1)').addClass('changeStyling');
+    });
+    $('.mouse-events__methods').mousemove(function(){
+        $('.list-items > li').filter(':eq(0)').addClass('changeOnDoubleClick');
+    });
+    $('.mouse-events__methods').mouseover(function(){
+        $('.list-items > li').filter(':lt(2)').addClass('changeOnMouseOver');
+    });
+    $('.mouse-events__methods').mouseleave(function(){
+        $('.list-items > li').filter(':gt(2)').addClass('changeOnMouseOut');
+        $('.list-items > li').filter(':gt(3)').click(function(){
+            $(this).mouseenter(function(){
+                console.log('mouseenter');
+            });
+        })
+    });
+});
 
 
 
