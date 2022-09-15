72471ff0187d89a97b9747ddf9a5be14feebb7d6
Jaber Khan <md.jaberkhan66@gmail.com>
2022-09-16 03:55:20 +0600

    Jquery-Based-Learning-Materials

--- 28.jQuery-Learning-Concepts/3.jQuery-Events-and-Events-Related-Materials/3.Jquery-Events-Helpers-Functions/4.jQuery-CSS-Manipulation-Methods.js
+++ 28.jQuery-Learning-Concepts/3.jQuery-Events-and-Events-Related-Materials/3.Jquery-Events-Helpers-Functions/4.jQuery-CSS-Manipulation-Methods.js
@@ -1,5 +1,16 @@
 /*
     jQuery CSS Manipulation Methods
     ===============================
-    
+    jQuery provides some CSS Manipulation Methods to work with the CSS styling properties and 
+    CSS class of the DOM elements.
+
+    CSS Manipulation Methods
+    ------------------------
+    1) css()         --> set style properties to the specified selector.
+    2) addClass()    --> add one or more class properties to the specified selector.
+    3) hasClass()    --> determining whether a specified selector has the assigned class property or not.
+    4) removeClass() --> remove single/multiple or even all classes properties from the specified selector.
+    5) toggleClass() --> toggling between adding or removing classes just like- addClass() and removeClass() methods.
+
 */
+
