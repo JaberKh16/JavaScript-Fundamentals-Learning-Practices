429289237dec2a04317998412b73907c1460839a
Jaber Khan <md.jaberkhan66@gmail.com>
2022-09-16 02:15:54 +0600

    Jquery-Based-Learning-Materials

---
+++ 28.jQuery-Learning-Concepts/3.jQuery-Events-and-Events-Related-Materials/3.Jquery-Events-Helpers-Functions/3.jQuery-HTML-Elements-Manipulation-Methods.js
@@ -0,0 +1,11 @@
+/*
+    jQuery HTML Elements Manipulation Methods
+    =========================================
+    jQuery provides a some methods related HTML Elements Manipulation to generally sets or gets value for
+    attribute, property, html or text.
+
+    HTML Elements Manipulation Methods
+    ----------------------------------
+    1) attr()   --> to get or set the value of specified attribute of the targetted element.
+    1) attr()   --> to get or set the value of specified attribute of the targetted element.
+*/
