4e095d9ec4498710f81ea30c5faf2335c2b94ecf
Jaber Khan <md.jaberkhan66@gmail.com>
2022-09-16 03:42:20 +0600

    Jquery-Based-Learning-Materials

--- 28.jQuery-Learning-Concepts/3.jQuery-Events-and-Events-Related-Materials/3.Jquery-Events-Helpers-Functions/3.jQuery-HTML-Elements-Manipulation-Methods.js
+++ 28.jQuery-Learning-Concepts/3.jQuery-Events-and-Events-Related-Materials/3.Jquery-Events-Helpers-Functions/3.jQuery-HTML-Elements-Manipulation-Methods.js
@@ -11,8 +11,12 @@
     2) prop()   --> to get or set the value of specified property of the targetted(DOM) element.
                     syntax- $('selector').prop('propname', 'propvalue')
     3) html()   --> to get or set the html content for the specified targetted(DOM) element.
+                    syntax- $('selector').html('content')
     4) text()   --> to get or set the text content for the specified targetted(DOM) element.
+                    syntax- $('selector').text('content')
     5) val()    --> to get or set the value property of the specified targetted(DOM) element.
+                    If a selector doesn't support value property then it return 'null'.
+                    syntax- $('selector').val('value'); 
 
 */
 
@@ -36,6 +40,23 @@ $('.html-method__section').ready(function(){
     // $('#div-element__html').html('class', 'changeDivElementStyling'); // setting a class attribute for the div element
     // console.log(`Setted property value is: ${$('#div-element__prop').prop('class')}`);
     // console.log(`Getting internal p tag default property value: ${$('p').prop('style')}`);
-    console.log($('#div-element__html').html()); // to the html() content initially inside this selector div
-    $('#div-element__html').html('<span>New Content Added</span>').appendTo('#div-element__html');
+    console.log($('#div-element__html').html()); // resulted the html content initially inside this selector div
+    $('#div-element__html').html('<span>New Content Added</span>');
+    console.log(`Getting html content: ${$('#div-element__html').html()}`);
+});
+
+
+// example of text() method
+$('.text-method__section').ready(function(){
+    console.log($('#div-element__text').text()); // resulted the text content initially inside this selector div
+    $('#div-element__text').text('<span>New Content Added</span>'); 
+    console.log(`Getting text content: ${$('#div-element__text').text()}`);
+});
+
+
+// examople of val() method
+$('.val-method__section').ready(function(){
+    console.log(`Initially the value is: ${$('#div-inputelement__val input:text').val()}`); // initially it is 'undefined'
+    $('#div-inputelement__val input:text').val('Mr. X');
+    console.log(`After setted the value is: ${$('#div-inputelement__val input:text').val()}`);
 });
