71efa25c3eebf0ab2c1a70638950414f0904356f
Jaber Khan <md.jaberkhan66@gmail.com>
2022-09-16 00:00:04 +0600

    Jquery-Based-Learning-Materials

--- 28.jQuery-Learning-Concepts/3.jQuery-Events-and-Events-Related-Materials/3.Jquery-Events-Helpers-Functions/2.jQuery-DOM-Manipulation-Helpers.js
+++ 28.jQuery-Learning-Concepts/3.jQuery-Events-and-Events-Related-Materials/3.Jquery-Events-Helpers-Functions/2.jQuery-DOM-Manipulation-Helpers.js
@@ -7,15 +7,42 @@
     jQuery DOM Manipulation Based Methods
     -------------------------------------
     1) append(element)      --> inserts element at the end of the elements which is specified by a selector.
-    2) appendTo(element)    --> 
-    3) before(element)      --> inserts content(new or existing DOM elements) before an element(s) which is 
+    2) prepend(element)     --> inserts content at the begining of the elements which is specified by a selector.
+    3) appendTo(element)    --> 
+    4) prependTo(element)   --> 
+    5) before(element)      --> inserts content(new or existing DOM elements) before an element(s) which is 
                                 specified by a selector.
-    4) after(element)       --> inserts content(new or existing DOM elements) after an element(s) which is
+    6) after(element)       --> inserts content(new or existing DOM elements) after an element(s) which is
                                 specified by a selector.
-    5) prepend(element)     --> inserts content at the begining of the elements which is specified by a selector.
-    6) remove(element)      --> removes element(s) from  the DOM with specified element.
-    7) wrap(element)        --> wrap an HTML structure around each element which is specified by selector.
-    8) replaceAll(element)  --> replace targetted element(s) with specified element. 
+    7) remove(element)      --> removes element(s) from  the DOM with specified element.
+    8) wrap(element)        --> wrap an HTML structure around each element which is specified by selector.
+    9) replaceAll(element)  --> replace targetted element(s) with specified element. 
 
-    
+
 */
+
+// example of after() method
+$('.afterMethodSection').ready(function(){
+
+    $('.div-element1').after('<ul><li>Item-1</li></ul>');
+});
+
+// example of before() method
+$('.beforeMethodSection').ready(function(){
+
+    $('.div-element2').before('<h2> Added h2 tag before p tag</h2>');
+});
+
+
+// example of append() method
+$('.prependMethodSection').ready(function(){
+    $('#list-items').append('<li>New Link Appended</li>');
+});
+
+
+// example of prepend() method
+$('.prependMethodSection').ready(function(){
+    $('#list-items').prepend('<li>New Link Prepended</li>');
+});
+
+
