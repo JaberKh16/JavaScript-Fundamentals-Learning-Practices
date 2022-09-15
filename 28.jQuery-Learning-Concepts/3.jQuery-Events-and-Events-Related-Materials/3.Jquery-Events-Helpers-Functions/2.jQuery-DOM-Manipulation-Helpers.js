fc1ffd20c42106fc9f6f9056b56dda8414dcb231
Jaber Khan <md.jaberkhan66@gmail.com>
2022-09-15 18:23:11 +0600

    Jquery-Based-Learning-Materials

--- 28.jQuery-Learning-Concepts/3.jQuery-Events-and-Events-Related-Materials/3.Jquery-Events-Helpers-Functions/2.jQuery-DOM-Manipulation-Helpers.js
+++ 28.jQuery-Learning-Concepts/3.jQuery-Events-and-Events-Related-Materials/3.Jquery-Events-Helpers-Functions/2.jQuery-DOM-Manipulation-Helpers.js
@@ -4,5 +4,18 @@
     jQuery can be used to manipulate DOM means to add, update and delete DOM element
     in the HTML page and for that jQuery provides some methods.
 
+    jQuery DOM Manipulation Based Methods
+    -------------------------------------
+    1) append(element)      --> inserts element at the end of the elements which is specified by a selector.
+    2) appendTo(element)    --> 
+    3) before(element)      --> inserts content(new or existing DOM elements) before an element(s) which is 
+                                specified by a selector.
+    4) after(element)       --> inserts content(new or existing DOM elements) after an element(s) which is
+                                specified by a selector.
+    5) prepend(element)     --> inserts content at the begining of the elements which is specified by a selector.
+    6) remove(element)      --> removes element(s) from  the DOM with specified element.
+    7) wrap(element)        --> wrap an HTML structure around each element which is specified by selector.
+    8) replaceAll(element)  --> replace targetted element(s) with specified element. 
+
     
 */
