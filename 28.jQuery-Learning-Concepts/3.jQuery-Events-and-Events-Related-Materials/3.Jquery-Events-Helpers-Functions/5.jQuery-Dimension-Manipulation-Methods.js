9e0bc49e1e6566cc1fa06d496d1209cccad23a29
Jaber Khan <md.jaberkhan66@gmail.com>
2022-09-16 08:46:35 +0600

    Jquery-Based-Learning-Materials

---
+++ 28.jQuery-Learning-Concepts/3.jQuery-Events-and-Events-Related-Materials/3.Jquery-Events-Helpers-Functions/5.jQuery-Dimension-Manipulation-Methods.js
@@ -0,0 +1,21 @@
+/*
+    jQuery Dimension Manipulation Methods
+    =====================================
+    jQuery library provides various methods to manipulate DOM elements dimension like- height, width, 
+    offset, positon etc.
+
+    Dimension Manipulation Methods
+    ------------------------------
+    1) height()         --> get or set the height of the specified selector. it takes an integer value as a parameter
+                            to that reference selector
+    2) width()          --> get or set the width of the specified selector.
+    3) innerHeight()    --> get or set the innerHeight(padding + height) of the specified selector.
+    4) innerWidth()     --> get or set the innerWidth(padding + width) of the specified selector.
+    5) outerHeight()    --> get or set the outerHeight(border + padding + height) of the specified selector.
+    6) outerWidth()     --> get or set the outerWidth(border + padding + width) of the specified selector.
+    7) offset()         --> get or set the left and top coordinates of the specified selector.
+    8) postion()        --> get or set the current coordinates of the specified selector.
+
+*/
+
+
