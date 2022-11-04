2e88181e301066a457fd74cefa232946173d44fd
Md. Jaber Khan <md.jaberkhan66@gmail.com>
2022-11-04 23:39:06 +0600

    JavaScript-Intermediate-Projects

--- 22.Practices-Simple-Projects/4.Contact-Form-Validation/scripts/contact-form-validation.js
+++ 22.Practices-Simple-Projects/4.Contact-Form-Validation/scripts/contact-form-validation.js
@@ -1,22 +1,25 @@
-function validatingInputField(){
+document.getElementsByClassName('submit-data')[0].addEventListener('click', function (event){
+    event.preventDefault();
     // getting all the input fields data
     const userName = document.getElementById('userName').value;
     const subjectMatter = document.getElementById('subjectMatter').value;
     const userEmail = document.getElementById('userEmail').value;
     const userMobile = document.getElementById('userMobile').value;
     const userMessage = document.getElementById('userMessage').value;
+    console.log(userName);
 
 
     // selecting the error message content field
     const errorMessage = document.getElementsByClassName('error-message')[0];
-    errorMessage.className = '.error-message';
-    const textContent = '';
+    errorMessage.classList.remove('error-message');
+    let textContent = '';
 
-    if(userName.length < 3){
+    if(userName.length < 3 && !Number.isInteger(userName.value)){
         textContent += 'Enter a valid username';
         errorMessage.innerText = textContent;
-        errorMessage.style.cssText = 'display:block padding:16px text-align:center';
+        // errorMessage.style.cssText = 'display:block padding:16px text-align:center';
+        errorMessage.classList.add('error-message');
         return false; 
     }
     return false;
-}
+});
