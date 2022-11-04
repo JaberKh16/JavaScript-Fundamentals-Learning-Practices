171831be7a2e11d228541ce31977ac2e6c4f1d5a
Md. Jaber Khan <md.jaberkhan66@gmail.com>
2022-11-05 01:33:49 +0600

    JavaScript-Intermediate-Projects

--- 22.Practices-Simple-Projects/4.Contact-Form-Validation/scripts/contact-form-validation.js
+++ 22.Practices-Simple-Projects/4.Contact-Form-Validation/scripts/contact-form-validation.js
@@ -11,15 +11,45 @@ document.getElementsByClassName('submit-data')[0].addEventListener('click', func
 
     // selecting the error message content field
     const errorMessage = document.getElementsByClassName('error-message')[0];
-    errorMessage.classList.remove('error-message');
+    // errorMessage.classList.remove('error-message');
     let textContent = '';
 
+    // validating the 'usernName' input field
     if(userName.length < 3 && !Number.isInteger(userName)){
-        textContent += 'Enter a valid username';
+        textContent = 'Enter a valid username';
         errorMessage.innerText = textContent;
         // errorMessage.style.cssText = 'display:block padding:16px text-align:center';
         errorMessage.classList.add('error-message');
         return false; 
+    }
+
+    // validating the 'subjectMatter' input field
+    if(subjectMatter.length < 5 && !Number.isInteger(subjectMatter.value)){
+        textContent ='Enter only a text message';
+        errorMessage.innerText = textContent;
+        errorMessage.classList.add('error-message');
+        return false;
+    }
+    // validating the 'userEmail' input field
+    if(userEmail.length <8  && !Number.isInteger(subjectMatter.value)){
+        textContent ='Enter a valid email';
+        errorMessage.innerText = textContent;
+        errorMessage.classList.add('error-message');
+        return false;
+    }
+    // validating the 'userMobile' input field
+    if(userMobile.length < 11 && Number.isInteger(subjectMatter.value)){
+        textContent ='Enter a valid mobile number';
+        errorMessage.innerText = textContent;
+        errorMessage.classList.add('error-message');
+        return false;
+    }
+    // validating the 'userMessage' input field
+    if(subjectMatter.length < 15 && !Number.isInteger(subjectMatter.value)){
+        textContent ='Enter only a text message';
+        errorMessage.innerText = textContent;
+        errorMessage.classList.add('error-message');
+        return false;
     }
     return false;
 });
