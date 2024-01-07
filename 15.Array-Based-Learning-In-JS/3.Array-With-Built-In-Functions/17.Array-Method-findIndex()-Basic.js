b9442bb81e02cc44e76827b75848e5a8017537f6
Jaber Khan <md.jaberkhan66@gmail.com>
2024-01-08 00:37:41 +0600

    Array-With-Built-In-Functions

---
+++ 15.Array-Based-Learning-In-JS/3.Array-With-Built-In-Functions/17.Array-Method-findIndex()-Basic.js
@@ -0,0 +1,22 @@
+/*
+    Array Method: array.findIndex(callbackFunction)
+    ===============================================
+    array.findIndex(callbackFunction) takes a callbackFunction and returns the
+    index value of the search element if found and otherwise returns -1.
+
+    findIndex() methods is generally used to find index value from a non primitive
+    datatypes- objects. This is the difference with indexOf() method with findIndex()
+    method.
+
+    In case of duplicate finding it only returns the first found item index.
+
+    Syntax:
+        array.findIndex(callbackFunction)
+
+*/
+
+const fruits = ['banana', 'apple', 'guava', 'pine-apple', 'apple', 'lichie'];
+const appleIndexValue = fruits.findIndex((fruitsItem) =>{
+    return fruitsItem === 'apple';
+});
+console.log('Found applie index is: ', appleIndexValue);
