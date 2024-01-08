8125e4f7a79bd24bd598f3e67aa5bcf589d05a8d
Jaber Khan <md.jaberkhan66@gmail.com>
2024-01-08 08:12:29 +0600

    Array-With-Built-In-Functions

--- 15.Array-Based-Learning-In-JS/3.Array-With-Built-In-Functions/16.Array-Method-flat()-Basic.js
+++ 15.Array-Based-Learning-In-JS/3.Array-With-Built-In-Functions/16.Array-Method-flat()-Basic.js
@@ -4,7 +4,17 @@
     flat() method takes an array and flattend it to its 1D array. It's flat everything inside even
     if have the any level order of array items and then flatted it to 1D array alike.
 
-    flat() doesn't take any parameters and as a return value returns the flatten array.
+    flat() doesn take single parameter but which is optional and as a return value 
+    returns the flatten array.
+
+    Syntax:
+        array.flat(level:optional)
+
+    'level' defines how deep level we wanted the array to be flatted means if-
+        1. array having 2D array - flatten to 1D
+        2. array having 3D array - need 'level' value 2 to flatten to 1D 
+    
+        and likewise.
 
 */
 
@@ -13,12 +23,9 @@
 
 // declaring the array
 const fruits = ['mango', 'apple', 'orange', 'pineapple', 'guava', [1, 2, 4, {user: '2', password: '123'}]];
-const flatted_fruits = fruits.flat();
-console.log(flatted_fruits);
+const flattedFruits = fruits.flat();
+console.log(flattedFruits);
 
-// const flatted_array = function(fruits){
-//     const flatted_array = fruits.flat();
-//     return flatted_array;
-// }
-
-// console.log(flatted_array());
+const randomItems = ['mango', 'apple', 'orange', 'pineapple', 'guava', [1, 2, 4, {user: '2', password: '123'}], [2, 23,[2, 'RJ', 'Car']]];
+const flattedRandomItems = randomItems.flat(2);
+console.log(flattedRandomItems);
