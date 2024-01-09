652b6734c0cbbf11517a309bc2cfba9b8f863154
Jaber Khan <md.jaberkhan66@gmail.com>
2024-01-09 06:28:31 +0600

    JavaScript-Functions-Concepts

--- 16.Function-Basic-In-JS/17.Closures-In-JS/1.Closure-In-JS.js
+++ 16.Function-Basic-In-JS/17.Closures-In-JS/1.Closure-In-JS.js
@@ -6,6 +6,8 @@
     where inner function has the lexical scope access to the parent functions like- chanining
     of access from inner to upwords. 
 
+    Note: Closure stores the reference of the lexical scope of the parent function block.
+
     Syntax:
         function parent(){
             var a = 10;
@@ -17,4 +19,48 @@
         }
         parent(); // invoking parent
         parent()(); // invoking inner
+
+
+    Uses Of Closures
+    ----------------
+    a. Module Design Pattern
+    b Cuurrying
+    c. Function like - once
+    d. Memoize
+    e. Maintaing state in async 
+    f. setTimeouts
+    g. Iterators
+    
 */
+
+// Example-1
+function parent(){
+    var a = 10;
+    function inner(){
+        console.log(a);
+    }
+    a = 100;
+    return inner
+}
+
+const callFunc = parent();
+console.log(callFunc);
+
+
+// Example-1
+function outerparent(){
+    var b = 200;
+    function parent(){
+        var a = 10;
+        function inner(){
+            console.log(a);
+        }
+        a = 100;
+        inner();
+    }
+    parent();
+    
+}
+
+const callFunc2 = outerparent();
+console.log(callFunc2);
