56febd49df47c352deb1e2d58027aaf97d71e2d5
Jaber Khan <md.jaberkhan66@gmail.com>
2024-01-11 11:08:26 +0600

    JavaScript-Functions-Concepts

---
+++ 16.Function-Basic-In-JS/19.Currying-In-JS/1.Currying-Basic-Concept.js
@@ -0,0 +1,58 @@
+/*
+    Currrying Basic Concepts
+    ========================
+    Currying is a concept in functional programming where a function is transformed into a sequence of functions, 
+    each taking a single argument. 
+    
+    The curried function returns a new function that expects the next argument, and this process continues 
+    until all arguments are supplied, at which point the original function is executed.
+
+*/
+
+// Presetted call, bind Version Of Currying
+function multiplication(num){
+    return num * 3;
+}
+
+const resultMult1 = multiplication.bind(this, 4);
+const resultMult2 = multiplication.bind(this, 5);
+console.log(resultMult1);
+console.log(resultMult2);
+
+
+// Example-2 Closure Version Of Currying
+// Curried version of the function
+function curryAdd(x) {
+    return function(y) {
+      return function(z) {
+        return x + y + z;
+      };
+    };
+  }
+  
+// Usage of the curried function
+const curriedAdd = curryAdd(2);
+const result = curriedAdd(3)(4);
+
+console.log(result); // Output: 9
+
+
+// Example-3 Generic Verison Of Curry Currying Another Function
+function curry(fn) {
+    return function curried(...args) {
+        if (args.length >= fn.length) {
+        return fn(...args);
+        } else {
+        return function (...nextArgs) {
+            return curried(...args, ...nextArgs);
+        };
+        }
+    };
+}
+  
+// Usage with the add function
+const curriedAdd2 = curry(add);
+const result2 = curriedAdd2(2)(3)(4);
+
+console.log(result2); // Output: 9
+  
