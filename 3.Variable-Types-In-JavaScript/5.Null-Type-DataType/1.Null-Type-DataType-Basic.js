bba7df20216ee2ca84e8e4213a0776cd20c07954
Jaber Khan <md.jaberkhan66@gmail.com>
2025-12-29 23:13:10 +0600

    JS-Variable-Types

--- 3.Variable-Types-In-JavaScript/5.Null-Type-DataType/1.Null-Type-DataType-Basic.js
+++ 3.Variable-Types-In-JavaScript/5.Null-Type-DataType/1.Null-Type-DataType-Basic.js
@@ -8,3 +8,8 @@ console.log(personal_info); // returns the object property with specified values
 // In JS, Null means nothing, it is basically a object datatype
 personal_info = null; // nulify the object thus it will get emptied
 console.log(personal_info); // this also return the object with no values inside it
+
+
+// check if the null and undefined is same
+let var;
+let var_null = null
