e448846ae7dd82d0d69743736e4ace6a03b103e1
Jaber Khan <md.jaberkhan66@gmail.com>
2026-03-27 07:57:59 +0600

    DSA-Sorting-Algorithms

--- 32.JavaScript-DSA-Practices/3.Sorting-Algorithms/6.radix-sort.js
+++ 32.JavaScript-DSA-Practices/3.Sorting-Algorithms/6.radix-sort.js
@@ -30,18 +30,18 @@ function radixSort(arr){
 
 console.log(radixSort([23, 345, 5467, 12, 2345, 9852]));
 
-// console.log(getDigits(12345, 0)); // 5
-// console.log(getDigits(12345, 1)); // 4
-// console.log(getDigits(12345, 2)); // 3
-// console.log(getDigits(12345, 3)); // 2
-// console.log(getDigits(12345, 4)); // 1
-// console.log(getDigits(12345, 5)); // 0
+console.log(getDigits(12345, 0)); // 5
+console.log(getDigits(12345, 1)); // 4
+console.log(getDigits(12345, 2)); // 3
+console.log(getDigits(12345, 3)); // 2
+console.log(getDigits(12345, 4)); // 1
+console.log(getDigits(12345, 5)); // 0
 
-// console.log(digitCount(1)); // 1
-// console.log(digitCount(25)); // 2
-// console.log(digitCount(314)); // 3
-// console.log(digitCount(0)); // 1
+console.log(digitCount(1)); // 1
+console.log(digitCount(25)); // 2
+console.log(digitCount(314)); // 3
+console.log(digitCount(0)); // 1
 
-// console.log(mostDigits([1234, 56, 7])); // 4
-// console.log(mostDigits([1, 1, 11111, 1])); // 5
-// console.log(mostDigits([12, 34, 56, 78])); // 2
+console.log(mostDigits([1234, 56, 7])); // 4
+console.log(mostDigits([1, 1, 11111, 1])); // 5
+console.log(mostDigits([12, 34, 56, 78])); // 2
