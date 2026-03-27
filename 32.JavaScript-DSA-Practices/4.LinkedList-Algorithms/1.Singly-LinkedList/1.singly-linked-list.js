1702b710ed1fa1bd9c35beeb64e17fa5112e1f9d
Jaber Khan <md.jaberkhan66@gmail.com>
2026-03-27 07:56:54 +0600

    DSA-LinkedList-Algorithms

--- 32.JavaScript-DSA-Practices/4.LinkedList-Algorithms/1.Singly-LinkedList/1.singly-linked-list.js
+++ 32.JavaScript-DSA-Practices/4.LinkedList-Algorithms/1.Singly-LinkedList/1.singly-linked-list.js
@@ -11,4 +11,77 @@ class SinglyLinkedList{
         this.tail = null;
         this.length = 0;
     }
+
+    push(val){
+        let newNode = new Node(val);
+        // if empty list, set head and tail to new node
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+    }
+    insetAtStart(val){
+        let newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+    }
+
+    insertAtEnd(val){
+        let newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+    }
+
+    popItem(){
+        if(!this.head) return undefined;
+        let current = this.head;
+        let newTail = current;
+        while(current.next){
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if(this.length === 0){
+            this.head = null;
+            this.tail = null;
+        }
+        return current.val;
+    }
 }
+
+let list = new SinglyLinkedList();
+
+// perform some operations on the list
+// list.head = new Node(10);
+// list.tail = list.head;
+// list.length++;
+
+// list.tail.next = new Node(5);
+// list.tail = list.tail.next;
+// list.length++;
+
+
+
+// perform push operations
+list.push(10);
+list.push(5);
+list.push(16);
+console.log(list);
